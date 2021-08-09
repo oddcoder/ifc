c61ef0338d555bd8848ce22e952672e908bfa3c9
Ahmed <ahmedsoliman@oddcoder.com>
2021-08-10 01:59:00 +0200

    Refactor tests so that it can be parallelized
    Signed-off-by: Ahmed <ahmedsoliman@oddcoder.com>

--- ifc-macros/test/build.rs
+++ ifc-macros/test/build.rs
@@ -1,78 +1,102 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+enum State {
+    Pass,
+    Fail,
+}
+
+lazy_static! {
+    static ref TESTS: Mutex<Vec<(State, &'static str)>> = Mutex::new(vec![
+        (State::Pass, "test/macro_unsafe.rs"),
+        (State::Pass, "test/macro_unsafe.rs"),
+        (State::Pass, "test/macros_pass.rs"),
+        (State::Pass, "test/assign_eq_declassify_to_low.rs"),
+        (State::Pass, "test/assign_eq_declassify_to_none.rs"),
+        (State::Pass, "test/assign_declassify_to_low.rs"),
+        (State::Pass, "test/assign_declassify_to_none.rs"),
+        (State::Pass, "test/let_declassify_to_low.rs"),
+        (State::Pass, "test/match_high_high.rs"),
+        (State::Pass, "test/match_low_high.rs"),
+        (State::Pass, "test/match_low_low.rs"),
+        (State::Pass, "test/match_low_none.rs"),
+        (State::Pass, "test/match_none_high.rs"),
+        (State::Pass, "test/match_none_low.rs"),
+        (State::Pass, "test/match_none_none.rs"),
+        (State::Pass, "test/while_high3.rs"),
+        (State::Pass, "test/while_none.rs"),
+        (State::Pass, "test/while_low.rs"),
+        (State::Pass, "test/if_else_high_guard.rs"),
+        (State::Pass, "test/if_else_none_guard.rs"),
+        (State::Pass, "test/if_else_low_guard.rs"),
+        (State::Pass, "test/nested_block_type_inference.rs"),
+        (State::Pass, "test/parens.rs"),
+        (State::Pass, "test/simple_block.rs"),
+        (State::Pass, "test/block_full_scope_test.rs"),
+        (State::Pass, "test/binary_high_high.rs"),
+        (State::Pass, "test/binary_high_low.rs"),
+        (State::Pass, "test/binary_high_none.rs"),
+        (State::Pass, "test/binary_low_high.rs"),
+        (State::Pass, "test/binary_low_low.rs"),
+        (State::Pass, "test/binary_low_none.rs"),
+        (State::Pass, "test/binary_none_high.rs"),
+        (State::Pass, "test/binary_none_low.rs"),
+        (State::Pass, "test/binary_none_none.rs"),
+        (State::Pass, "test/format_lowvars.rs"),
+        (State::Pass, "test/define_high_as_low.rs"),
+        (State::Pass, "test/assign_untyped_to_untyped.rs"),
+        (State::Pass, "test/assign_low_to_untyped.rs"),
+        (State::Pass, "test/assign_untyped_to_low.rs"),
+        (State::Pass, "test/assign_low_to_low.rs"),
+        (State::Pass, "test/assign_untyped_to_high.rs"),
+        (State::Pass, "test/assign_low_to_high.rs"),
+        (State::Pass, "test/assign_high_to_high.rs"),
+        (State::Pass, "test/pass_low_to_fn.rs"),
+        (State::Pass, "test/pass_high_unsafe_to_fn.rs"),
+        (State::Pass, "test/unary.rs"),
+        (State::Pass, "test/assignop_untyped_to_untyped.rs"),
+        (State::Pass, "test/assignop_low_to_untyped.rs"),
+        (State::Pass, "test/assignop_untyped_to_high.rs"),
+        (State::Pass, "test/assignop_low_to_high.rs"),
+        (State::Pass, "test/assignop_high_to_high.rs"),
+        (State::Pass, "test/assignop_untyped_to_low.rs"),
+        (State::Pass, "test/assignop_low_to_low.rs"),
+        (State::Fail, "test/macros_fail.rs"),
+        (State::Fail, "test/useless_declassify.rs"),
+        (State::Fail, "test/match_high_low.rs"),
+        (State::Fail, "test/match_high_none.rs"),
+        (State::Fail, "test/while_high2.rs"),
+        (State::Fail, "test/while_high1.rs"),
+        (State::Fail, "test/if_else_high_guard4.rs"),
+        (State::Fail, "test/if_else_high_guard3.rs"),
+        (State::Fail, "test/if_else_high_guard2.rs"),
+        (State::Fail, "test/nested_block_type_inference2.rs"),
+        (State::Fail, "test/block_access_upper_scope.rs"),
+        (State::Fail, "test/variables.rs"),
+        (State::Fail, "test/format_highvars.rs"),
+        (State::Fail, "test/define_low_as_high.rs"),
+        (State::Fail, "test/pass_high_to_fn.rs"),
+        (State::Fail, "test/pass_high_mut_to_fn.rs"),
+        (State::Fail, "test/pass_high_ref_to_fn.rs"),
+        (State::Fail, "test/unused_attribute.rs"),
+        (State::Fail, "test/assign_high_to_untyped.rs"),
+        (State::Fail, "test/assign_high_to_low.rs"),
+        (State::Fail, "test/assignop_high_to_untyped.rs"),
+        (State::Fail, "test/assignop_high_to_low.rs"),
+    ]);
+}
+
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.pass("test/macro_unsafe.rs");
-    t.compile_fail("test/macros_fail.rs");
-    t.pass("test/macros_pass.rs");
-    t.pass("test/assign_eq_declassify_to_low.rs");
-    t.pass("test/assign_eq_declassify_to_none.rs");
-    t.pass("test/assign_declassify_to_low.rs");
-    t.pass("test/assign_declassify_to_none.rs");
-    t.pass("test/let_declassify_to_low.rs");
-    t.compile_fail("test/useless_declassify.rs");
-    t.pass("test/match_high_high.rs");
-    t.compile_fail("test/match_high_low.rs");
-    t.compile_fail("test/match_high_none.rs");
-    t.pass("test/match_low_high.rs");
-    t.pass("test/match_low_low.rs");
-    t.pass("test/match_low_none.rs");
-    t.pass("test/match_none_high.rs");
-    t.pass("test/match_none_low.rs");
-    t.pass("test/match_none_none.rs");
-    t.pass("test/while_high3.rs");
-    t.compile_fail("test/while_high2.rs");
-    t.compile_fail("test/while_high1.rs");
-    t.pass("test/while_none.rs");
-    t.pass("test/while_low.rs");
-    t.compile_fail("test/if_else_high_guard4.rs");
-    t.compile_fail("test/if_else_high_guard3.rs");
-    t.compile_fail("test/if_else_high_guard2.rs");
-    t.pass("test/if_else_high_guard.rs");
-    t.pass("test/if_else_none_guard.rs");
-    t.pass("test/if_else_low_guard.rs");
-    t.pass("test/nested_block_type_inference.rs");
-    t.compile_fail("test/nested_block_type_inference2.rs");
-    t.pass("test/parens.rs");
-    t.pass("test/simple_block.rs");
-    t.compile_fail("test/block_access_upper_scope.rs");
-    t.pass("test/block_full_scope_test.rs");
-    t.pass("test/binary_high_high.rs");
-    t.pass("test/binary_high_low.rs");
-    t.pass("test/binary_high_none.rs");
-    t.pass("test/binary_low_high.rs");
-    t.pass("test/binary_low_low.rs");
-    t.pass("test/binary_low_none.rs");
-    t.pass("test/binary_none_high.rs");
-    t.pass("test/binary_none_low.rs");
-    t.pass("test/binary_none_none.rs");
-    t.compile_fail("test/variables.rs");
-    t.pass("test/format_lowvars.rs");
-    t.compile_fail("test/format_highvars.rs");
-    t.pass("test/define_high_as_low.rs");
-    t.compile_fail("test/define_low_as_high.rs");
-    t.pass("test/assign_untyped_to_untyped.rs");
-    t.pass("test/assign_low_to_untyped.rs");
-    t.compile_fail("test/assign_high_to_untyped.rs");
-    t.pass("test/assign_untyped_to_low.rs");
-    t.pass("test/assign_low_to_low.rs");
-    t.compile_fail("test/assign_high_to_low.rs");
-    t.pass("test/assign_untyped_to_high.rs");
-    t.pass("test/assign_low_to_high.rs");
-    t.pass("test/assign_high_to_high.rs");
-    t.pass("test/pass_low_to_fn.rs");
-    t.pass("test/pass_high_unsafe_to_fn.rs");
-    t.compile_fail("test/pass_high_to_fn.rs");
-    t.compile_fail("test/pass_high_mut_to_fn.rs");
-    t.compile_fail("test/pass_high_ref_to_fn.rs");
-    t.compile_fail("test/unused_attribute.rs");
-    t.pass("test/unary.rs");
-    t.pass("test/assignop_untyped_to_untyped.rs");
-    t.pass("test/assignop_low_to_untyped.rs");
-    t.compile_fail("test/assignop_high_to_untyped.rs");
-    t.pass("test/assignop_untyped_to_low.rs");
-    t.pass("test/assignop_low_to_low.rs");
-    t.compile_fail("test/assignop_high_to_low.rs");
-    t.pass("test/assignop_untyped_to_high.rs");
-    t.pass("test/assignop_low_to_high.rs");
-    t.pass("test/assignop_high_to_high.rs");
+    loop {
+        let (state, path) = match TESTS.lock().unwrap().pop() {
+            Some(test) => test,
+            None => break,
+        };
+        match state {
+            State::Pass => t.pass(path),
+            State::Fail => t.compile_fail(path),
+        }
+    }
 }
